c2f22e98dfe9a8444ef7e538c7dddfb74efbef42
Nathan Classen <nclassen@techinsights.com>
2022-05-12 12:23:11 -0600

    redid problem 3

--- 3_longestSubstringWithoutRepeatingCharacters/main.go
+++ 3_longestSubstringWithoutRepeatingCharacters/main.go
@@ -1,134 +1,94 @@
 /*
-	Given a string `s`, find the length of the longest substring without repeating characters.
-
+	Given a string s, find the length of the longest substring without repeating characters.
 
 	Example 1:
-
+                l
 	Input: s = "abcabcbb"
+                r
+
 	Output: 3
 	Explanation: The answer is "abc", with the length of 3.
 
 	Example 2:
-
 	Input: s = "bbbbb"
 	Output: 1
 	Explanation: The answer is "b", with the length of 1.
 
 	Example 3:
-
 	Input: s = "pwwkew"
 	Output: 3
 	Explanation: The answer is "wke", with the length of 3.
 	Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
 	Constraints:
+		0 <= s.length <= 5 * 104
+		s consists of English letters, digits, symbols and spaces.
+*/
+/*
+	Problem:
+	find the longest string of unique characters within a string
+	basically need to check from each position of the string
+	if there is only one char in str, thats the longest
+	if no chars, 0 is the answer
 
-    0 <= s.length <= 5 * 104
-    s consists of English letters, digits, symbols and spaces.
-	___________________________________________________________________________________________
+	Mental model:
+	track the longest sequence found, as you iterate string from each character as a starting position,
+	checking how many consecutive chars from each starting position are unique
 
-	- looking for longest sequence of chars, where current char has not been seen before
-	- gotchas:
-		- we may find a sequence of 4 chars starting from the first char, but then may find a unique sequence of 5 starting from the 2nd or 3rd char, etc
-			- so, at first glance, seems we will need to examine entire string (breaking from examination upon first repeated char) starting from each char
-			- can stop looking if `currentLongest` >= remaining characters in string from current starting place
+	Datastructure:
+	iteration and length tracking can be done with a growing and sliding window
+	may use map to track seen chars
 
-	- will need a way to track `seen` chars
-		- map of map[string]bool seems a good choice
-
-		would have to somehow "clear" the map after each "outer" iteration...
-		alternatively, could keep track of an int value with each outer iteration `0` on one iteration, then `1` on the next, and so on, so that "tuthiness" is held as "whatever the current"
-		`int` tracker is. This would clearing un-necessary. Or could just rest map to empty map... certainly simpler and less space
-	- will have to track longest, and a current longest variable
-		- at the end of a partial iteration, only replace longest if current longest is greater
-
-	Algo:
-
-		general outline:
-			- loop while tracking to pointers
-			- 1st theme is recording, eval, processing
-			- 2nd theme is loop status control via pointer manipulation
-				if `criteria` r = n
-				if `criteria` l = x
-				if `criteria` break/return
-
-
-		set all variables
-			- l int 0
-			- r int 0
-			- localLongest int
-			- globalLongest int
-			- seen map[byte]true
-
-		for
-			see char at r
-				if char not seen
-					add to seen
-					incr localL
-					incr r
-				if char seen
-					clear seen
-					maybe replace globalL
-					clear localL
-					l++
-					r = l
-
-				if length of globalL is larger than remaining chars
-					break
-
-				if l >= len(s)
-					break
+	algorithm:
+	set l, r to 0
+	set longest to 0
+	as long as `l` is less than len(str)
+		if r is >= len(str)
+			l++
+			r = l
+			globalL = max(longest, globalL)
+		check if r char has been seen
+			if not
+				++longest
+				++r
+			if so
+				l++
+				r = l
+			globalL = max(longest, globalL)
 
 */
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println(lengthOfLongestSubstring("aacabczc"))
+	fmt.Println(lengthOfLongestSubstring("asdfghjklqwertyuiopzxcvbnm"))
 }
 
 func lengthOfLongestSubstring(s string) int {
-	l := 0
-	r := 0
-	localL := 0
-	globalL := 0
+	globL := 0
+	currL := 0
 	seen := map[byte]bool{}
-	sLen := len(s)
+	l, r := 0, 0
 
-	if sLen == 0 {
-		return 0
-	}
-
-	for {
-		char := s[r]
-		if _, haveSeen := seen[char]; haveSeen {
-			seen = map[byte]bool{}
-			if localL > globalL {
-				globalL = localL
-			}
-			localL = 0
+	for l < len(s) && len(s)-l > globL {
+		if r >= len(s) || seen[s[r]] {
 			l++
 			r = l
-
-			if globalL >= sLen-l {
-				break
-			}
-		} else {
-			seen[char] = true
-			localL++
-			r++
+			globL = int(math.Max(float64(currL), float64(globL)))
+			currL = 0
+			seen = map[byte]bool{}
+			continue
 		}
-
-		if l >= sLen || r >= sLen {
-			break
-		}
+		seen[s[r]] = true
+		currL++
+		r++
 	}
-
-	if globalL > localL {
-		return globalL
-	} else {
-		return localL
-	}
+	return globL
 }
+
+// abca
